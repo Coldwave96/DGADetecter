a96cfb37895e6296d9e6c0592e8dc1eddeb35c61
c01dsnap <c01dsnap.max@gmail.com>
2023-08-14 15:50:10 +0800

    Update new features

--- feature_generater.py
+++ feature_generater.py
@@ -1,7 +1,8 @@
 import math
+import textstat
 
 # Information Entropy
-def info_entropy(data):
+def cal_entropy(data):
     if not data:
         return 0
     valid_chars = set(data)
@@ -13,3 +14,27 @@ def info_entropy(data):
             entropy += - p_x * math.log(p_x, 2)
     
     return entropy
+
+# Readability
+def cal_Readability(data):
+    score = textstat.flesch_reading_ease(data)
+    return score
+
+# Weights of vowel characters
+def cal_vowel(data):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    vowel_count = 0
+    total_chars = 0
+
+    for char in data:
+        if char.isalpha():
+            total_chars += 1
+            if char in vowels:
+                vowel_count += 1
+    
+    if total_chars > 0:
+        vowel_ratio = vowel_count / total_chars
+        return vowel_ratio
+    else:
+        return 0.0
---
+++ gibberish_train.py
@@ -0,0 +1,55 @@
+import math
+import pickle
+
+accepted_chars = 'abcdefghijklmnopqrstuvwxyz '
+pos = dict([(char, idx) for idx, char in enumerate(accepted_chars)])
+
+# Return only the subset of chars from acceptrd_chars
+# Ignore punctuation, symbols, etc.
+def normalize(line):
+    return [c.lower() for c in line if c.lower() in accepted_chars]
+
+# Return all n-grams fron normalized line
+def ngram(n, line):
+    normalized_line = normalize(line)
+    for start in range(0, len(normalized_line) - n + 1):
+        yield "".join(normalized_line[start, start + n])
+
+# Return the average transition prob from line through log_prob_mat.
+def avg_transition_prob(line, log_prob_mat):
+    log_prob = 0.0
+    transition_ct = 0
+    for a, b in ngram(2, line):
+        log_prob += log_prob_mat[pos[a]][pos[b]]
+        transition_ct += 1
+    # The exponentiation translates from log probs to probs.
+    return math.exp(log_prob / (transition_ct or 1))
+
+def train():
+    k = len(accepted_chars)
+    counts = [[10 for i in range(k)] for i in range(k)]
+
+    # Count transitions from big text file
+    for line in open('Datasets/Words/Gibberish/gibberish.txt'):
+        for a, b in ngram(2, line):
+            counts[pos[a]][pos[b]] += 1
+    
+    # Normalize the counts via log probabilities
+    for _, row in enumerate(counts):
+        s = float(sum(row))
+        for j in range(len(row)):
+            row[j] = math.log(row[j] / s)
+    
+    # Find the probability of generating a few arbitrarily choosen good and bad phrases.
+    good_probs = [avg_transition_prob(line, counts) for line in open('Datasets/Words/Gibberish/good.txt')]
+    bad_probs = [avg_transition_prob(line, counts) for line in open('Datasets/Words/Gibberish/bad.txt')]
+
+    # Assert that we actually are capable of detecting the junk.
+    assert min(good_probs) > max(bad_probs)
+
+    # And pick a threshold halfway between the worst good and best bad inputs.
+    thresh = (min(good_probs) + max(bad_probs)) / 2
+    pickle.dump({'mat': counts, 'thresh': thresh}, open('Outputs/Gibberish/gib_model.pki', 'wb'))
+
+    if __name__ == '__main__':
+        train()
